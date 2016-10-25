fe912787c089fb786ed3a5680ff3505e7dba1ef0
jpmmaia <jpmmaia@gmail.com>
2016-10-25 12:40:02 +0200

    - Drawing spheres in view space.

--- RTVisTask1/src/shader/molecules.glsl
+++ RTVisTask1/src/shader/molecules.glsl
@@ -38,36 +38,36 @@ in vec3 vs_out_color[];
 in float vs_out_radius[];
 
 // Output:
-out vec3 gs_out_positionW;
+out vec3 gs_out_positionV;
 flat out vec3 gs_out_color;
-flat out float gs_out_radius;
-flat out vec3 gs_out_centerW;
+flat out float gs_out_radiusV;
+flat out vec3 gs_out_centerV;
 
 void main()
 {
-	vec3 centerW = vs_out_positionW[0];
-	float radius = vs_out_radius[0];
+	vec3 centerV = (u_viewMatrix * vec4(vs_out_positionW[0], 1.0f)).xyz;
+	float radiusV = vs_out_radius[0];
 	
 	// Calculate the local coordinate system:
 	vec3 up = vec3(0.0f, 1.0f, 0.0f);
-	vec3 forward = normalize(u_eyePositionW - centerW);
+	vec3 forward = normalize(vec3(0.0f, 0.0f, 0.0f) - centerV);
 	vec3 right = cross(up, forward);
 
 	// Compute the position of the vertices of the billboard:
-	vec4 vertices[4];
-	vertices[0] = vec4(centerW - up * radius - right * radius, 1.0f);
-	vertices[1] = vec4(centerW - up * radius + right * radius, 1.0f);
-	vertices[2] = vec4(centerW + up * radius - right * radius, 1.0f);
-	vertices[3] = vec4(centerW + up * radius + right * radius, 1.0f);
+	vec4 verticesV[4];
+	verticesV[0] = vec4(centerV - up * radiusV - right * radiusV, 1.0f);
+	verticesV[1] = vec4(centerV - up * radiusV + right * radiusV, 1.0f);
+	verticesV[2] = vec4(centerV + up * radiusV - right * radiusV, 1.0f);
+	verticesV[3] = vec4(centerV + up * radiusV + right * radiusV, 1.0f);
 
 	// Emit vertices:
 	for (int i = 0; i < 4; ++i)
 	{
-		gl_Position = u_viewProjectionMatrix * vertices[i];
-		gs_out_positionW = vertices[i].xyz;
+		gl_Position = u_projectionMatrix * verticesV[i];
+		gs_out_positionV = verticesV[i].xyz;
 		gs_out_color = vs_out_color[0];
-		gs_out_radius = vs_out_radius[0];
-		gs_out_centerW = centerW;
+		gs_out_radiusV = radiusV;
+		gs_out_centerV = centerV;
 		EmitVertex();
 	}
 	EndPrimitive();
@@ -76,18 +76,18 @@ void main()
 --Fragment
 
 // Input:
-in vec3 gs_out_positionW;
+in vec3 gs_out_positionV;
 flat in vec3 gs_out_color;
-flat in float gs_out_radius;
-flat in vec3 gs_out_centerW;
+flat in float gs_out_radiusV;
+flat in vec3 gs_out_centerV;
 
 // Output:
 out vec4 fs_out_color;
 
 void main()
 {
-	vec3 fromCenterVector = gs_out_positionW - gs_out_centerW;
-	if (dot(fromCenterVector, fromCenterVector) > gs_out_radius * gs_out_radius)
+	vec3 fromCenterVector = gs_out_positionV - gs_out_centerV;
+	if (dot(fromCenterVector, fromCenterVector) > gs_out_radiusV * gs_out_radiusV)
 		discard;
 
 	fs_out_color = vec4(gs_out_color, 1.0f);
